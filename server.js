dea0268cfee4c039ab0afc950431baa85f91badc
NirojEdulab <niroj@edulab.in>
2022-12-10 17:18:45 +0530

    Boilerplate server setup

---
+++ server.js
@@ -0,0 +1,8 @@
+const express = require("express");
+
+const app = express();
+
+
+app.listen(3000); //3 - listen for any incoming requests
+
+console.log('server running on http://localhost:3000/')
